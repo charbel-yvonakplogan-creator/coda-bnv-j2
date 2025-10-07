0e1b715ab2de42cee0a1f1d4876403c646414627
Charbelakplogan2002 <acharbel02@gmail.com>
2025-10-07 12:17:39 +0000

    exercice 5

---
+++ ex-5/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main () {
+
+    double a = 12;
+    double b =7;
+
+    double sum = a + b;
+
+    double moyenne = sum / 2;
+
+    printf("La moyenne de 12 et 7 est égale à %.2f\n", moyenne);
+    exit(0);
+
+}
