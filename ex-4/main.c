5a7f0b047573bc2bde06339123c9a662260c940a
Charbelakplogan2002 <acharbel02@gmail.com>
2025-10-07 12:01:54 +0000

    exercice 4

---
+++ ex-4/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    
+    int x = 6;
+    int y = 7;
+
+    int produit = x * y;
+
+    printf("%d x %d = %d\n", x, y, produit);
+    exit(0);
+}
